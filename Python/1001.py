600595c85ea879172157ffe4a249162fe84853a1
FelipedeLimaDeveloper <felipedelima.developer@gmail.com>
2017-10-04 10:21:04 -0300

    Python

---
+++ Python/1001.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  4 09:47:26 2017
+
+@author: felipe
+"""
+
+a = int(input())
+b = int(input())
+
+sum = a + b
+
+print("X =",sum)
